6b2b2aa8579f0b9151dc74c8e9d2276005db59b6
Thomas Wilson <twilson.spqr@gmail.com>
2017-10-19 23:14:30 -0400

    sweeping

--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    sequence(:email) { |n| "email#{n}@#{Faker::Lovecraft.location}.com" }
-    sequence(:username) { |n| "#{Faker::Number.number(15)}" }
-    password "#{Faker::Lovecraft.word}#{Faker::Lovecraft.word}"
-  end
-end
